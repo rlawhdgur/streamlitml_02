f91c1339993b80eb4f71328ae2035e3dfd2c2b2d
rlawhdgur <dlsrks_25@naver.com>
2023-01-31 12:42:28 +0900

    updated

---
+++ app.py
@@ -0,0 +1,26 @@
+# -*- coding : utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+import matplotlib as mpl
+import seaborn as sns
+import plotly
+# import sklearn
+import numpy as np
+
+
+def main():
+
+    # 코드작성
+    st.title("streamlit : " + str(st.__version__))
+    st.title("pandas : " + str(pd.__version__))
+    st.title("matplotlib : " + str(mpl.__version__))
+    st.title("seaborn : " + str(sns.__version__))
+    # st.title("plotly : " + str(sklearn.__version))
+    st.title("sklearen : " + str(np.__version__))
+    st.title("numpy : " + str(plotly.__version__))
+
+
+if __name__ == "__main__":
+    main()
+    
